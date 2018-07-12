9d54d475a57eb94878fc1d6ff5b736a832937fd2
stan <s.k.golovin@gmail.com>
2018-07-12 13:50:48 +0700

    zcash changed to CrypticCoin

--- benchmarks/index.js
+++ benchmarks/index.js
@@ -5,7 +5,7 @@ var bitcoin = require('bitcoin');
 var async = require('async');
 var maxTime = 20;
 
-console.log('Zcash Service native interface vs. Zcash JSON RPC interface');
+console.log('CrypticCoin Service native interface vs. CrypticCoin JSON RPC interface');
 console.log('----------------------------------------------------------------------');
 
 // To run the benchmarks a fully synced Bitcore Core directory is needed. The RPC comands
@@ -43,12 +43,12 @@ bitcoind.start(function(err) {
   if (err) {
     throw err;
   }
-  console.log('Zcash started');
+  console.log('CrypticCoin started');
 });
 
 bitcoind.on('ready', function() {
 
-  console.log('Zcash ready');
+  console.log('CrypticCoin ready');
 
   var client = new bitcoin.Client({
     host: 'localhost',
--- regtest/cluster.js
+++ regtest/cluster.js
@@ -6,7 +6,7 @@ var spawn = require('child_process').spawn;
 
 var BitcoinRPC = require('bitcoind-rpc');
 var rimraf = require('rimraf');
-var bitcore = require('bitcore-lib-zcash');
+var bitcore = require('bitcore-lib-crypticcoin');
 var chai = require('chai');
 var should = chai.should();
 
