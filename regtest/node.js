6504b9bf485e92d54d4b1bdda6751449e3ee5bf6
stan <s.k.golovin@gmail.com>
2018-07-12 13:50:48 +0700

    zcash changed to CrypticCoin

--- regtest/node.js
+++ regtest/node.js
@@ -9,7 +9,7 @@ var log = index.log;
 log.debug = function() {};
 
 var chai = require('chai');
-var bitcore = require('bitcore-lib-zcash');
+var bitcore = require('bitcore-lib-crypticcoin');
 var rimraf = require('rimraf');
 var node;
 
